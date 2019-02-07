af9c5d39fd84abee1c6c8bbc8e8ec2dfea12b85d
Alex Karelin <alex@karel.in>
2019-02-07 08:56:49 -0800

    190207 08:56:49

--- gitcommit.sh
+++ gitcommit.sh
@@ -1,8 +1,9 @@
 git add .
+git add PACKAGES/
 git status
 git commit -m "$(date '+%y%m%d %H:%M:%S')"
 git push origin master
 
-git config credential.helper store
+#git config credential.helper store
 #git config --global credential.helper 'cache --timeout 
 
